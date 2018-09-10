c148a740e51b04a7a3ce3102ea529f52cf06e336
afifkhaidir-tkpd <khaidir.afif@tokopedia.com>
2018-09-10 13:48:13 +0700

    Clean file structure to as minimum as possible

--- config.js
+++ config.js
@@ -3,10 +3,6 @@ const config = {
     main: [
       './src/js/index.js',
       './src/sass/style.scss',
-      './src/js/jqueryui.js',
-    ],
-    unify: [
-      './src/js/unify.js',
     ],
   },
   jquery: true,
@@ -16,37 +12,12 @@ const config = {
       filename: 'index.html',
       chunks: ['main'],
     },
-    {
-      template: './src/example/buttons.html',
-      filename: 'example/buttons.html',
-      chunks: ['main', 'unify'],
-    },
-    {
-      template: './src/example/index.html',
-      filename: 'example/index.html',
-      chunks: ['main'],
-    },
-    {
-      template: './src/example/form.html',
-      filename: 'example/form.html',
-      chunks: ['main', 'unify'],
-    },
-    {
-      template: './src/example/selector.html',
-      filename: 'example/selector.html',
-      chunks: ['main', 'unify'],
-    },
-    {
-      template: './src/example/cards.html',
-      filename: 'example/cards.html',
-      chunks: ['main'],
-    },
   ],
   cdn: {
     upload: false,
     options: {
       bucket: 'tokopedia-upload',
-      directory: 'assets-tokopoints/prod/webpacktest/',
+      directory: 'microsite-production/{campaign}/{year}/',
       domain: 'https://ecs7.tokopedia.net/',
     },
   },
